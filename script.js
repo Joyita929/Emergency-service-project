6375309cb40e402f3e8cbee1108d6317a8855116
joyita <joyita@example.com>
2025-09-19 11:43:39 +0600

    added history section

--- script.js
+++ script.js
@@ -10,3 +10,91 @@ for (const heart of whiteHeart) {
     heartCountFinal.innerText = heartCount;
   });
 }
+// call button deal - common part
+const greenCall = document.getElementsByClassName("greenCall");
+const coinCountFinal = document.getElementById("coinCountFinal");
+//let coinCount = 100;
+let coinCount = parseInt(coinCountFinal.innerText);
+//console.log("initial coin count is: " + coinCount);
+let callHistoryList = [];
+for (const call of greenCall) {
+  call.addEventListener("click", function (event) {
+    if (coinCount < 20)
+      alert(
+        "❌ You don't have enough balance to call. To make a successful call, you need at least 20 coins. "
+      );
+    else {
+      coinCount = coinCount - 20;
+      //alert(coinCount);
+      coinCountFinal.innerText = coinCount;
+
+      callAlert(event.target.id);
+    }
+  });
+}
+
+// Function to handle call alert
+function callAlert(id) {
+  const clicked = document.getElementById(id);
+
+  // parent container (where h2 and p live)
+  let parentDiv;
+  if (id === "card1") {
+    parentDiv = clicked.parentElement;
+  } else if (id === "card2") {
+    parentDiv = clicked.parentElement.parentElement;
+  } else if (id === "card3") {
+    parentDiv = clicked.parentElement.parentElement;
+  } else if (id === "card4") {
+    parentDiv = clicked.parentElement.parentElement;
+  }
+  if (id === "card5") {
+    parentDiv = clicked.parentElement;
+  } else if (id === "card6") {
+    parentDiv = clicked.parentElement.parentElement;
+  } else if (id === "card7") {
+    parentDiv = clicked.parentElement.parentElement;
+  } else if (id === "card8") {
+    parentDiv = clicked.parentElement.parentElement;
+  } else if (id === "card9") {
+    parentDiv = clicked.parentElement.parentElement;
+  }
+  // find h2 and p inside that parent
+  const h2Text = parentDiv.querySelector("h2")?.innerText || ""; //Looks inside parentDiv. Finds the first <h2> element inside it. If there’s no <h2>, it returns null.
+  const pText = parentDiv.querySelector("p")?.innerText || "";
+
+  alert("📞Calling " + pText + " " + h2Text + "...");}
+  //call history
+  const data = {
+    service: pText,
+    cellNo: h2Text,
+    date: new Date().toLocaleTimeString(),
+  };
+
+  callHistoryList.push(data);
+  console.log(callHistoryList);
+  const callHistoryRecord = document.getElementById("callHistoryRecord");
+  const div = document.createElement("div");
+  div.style.backgroundColor = " #f5f6fa";
+  div.style.margin = "3px";
+  div.style.marginTop = "15px";
+  div.style.padding = "5px";
+  div.innerHTML = `
+   <div style="display:flex ;align-items:center; justify-content: space-between">
+    <div>
+        <p>${data.service}</p>
+        <p>${data.cellNo}</p>
+    </div>
+
+    <div> <p>${data.date}</p></div>
+   </div>
+    `;
+  callHistoryRecord.appendChild(div);
+
+  //clear button of callHistory
+
+  const clearButton = document.getElementById("clearButton");
+  clearButton.addEventListener("click", function () {
+    callHistoryList = [];
+    callHistoryRecord.innerHTML = "";
+  });
